50dbd82d507b673d1e8588af7a2ae69c2605669a
dlc318 <dlc318@lehigh.edu>
2014-08-29 17:14:35 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+////////////////////
+//Dana Cunningham
+//
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+    //  add main method
+      public static void main(String[] args) {
+          
+        //  print the statement with box around it
+            System.out.println("  - - - - - - - - - - -");
+            System.out.println("  |   W E L C O M E   |");
+            System.out.println("  - - - - - - - - - - -");
+            System.out.println("  ^   ^   ^   ^   ^   ^");
+            System.out.println(" / \\ / \\ / \\ / \\ / \\ / \\ ");
+            System.out.println("|-D- -L- -C- -3- -1- -8-|");
+            System.out.println(" \\ / \\ / \\ / \\ / \\ / \\ / ");
+            System.out.println("  v   v   v   v   v   v");
+            System.out.println("Last year I traveled to China and climbed the Great Wall.");
+            
+            //test/testtest
+      }
+}
