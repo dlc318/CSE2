6a84396005be9a68cd23008e6bca56295d347f10
dlc318 <dlc318@lehigh.edu>
2014-10-01 00:03:29 +0000

    updated BoolaBoola

--- hw05/BoolaBoola.java
+++ hw05/BoolaBoola.java
@@ -25,185 +25,190 @@ public class BoolaBoola{    //creates public class
         
         
         
-        if(value4==1 && value5==1){//first if statement //with two && operators
+        if(value4==1 && value5==1){//if statement 1 //with two && operators
             
             boolean result1=(value1 && value2 && value3);    //result with true/false values
             
             System.out.println("Does " +value1+ " && " +value2+ " && " +value3+ " have the value true(t/T) or false(f/F)?");    //prompts user to state whether value is true or false
             
-            if(result1==true){//second if statement   //if result is true
+            if(result1==true){//if statement 2   //if result is true
                 
-                if(myScanner.hasNext()){    //if user enters a String
+                if(myScanner.hasNext()){    //if statement 3   //if user enters a String
                     answer=myScanner.next();    //sets answer equal to user input
                 
-                        if(answer.equals("t") || answer.equals("T")){//third if statement     //if user enters correct answer
+                        if(answer.equals("t") || answer.equals("T")){//if statement 4    //if user enters correct answer
                             
                                 System.out.println("Correct");
                                 return;//leaves the program
-                        }//ends third if statement
+                        }//if statement 4
                         
-                        else{//first else statement     //when user does not enter correct anwer
+                        else{//else statement 1    //when user does not enter correct anwer
                               
                                 System.out.println("Wrong; try again");//notifies user of wrong answer
                                 return;//leaves the program
-                        }//ends first else statement 
-                }
+                        }//else statement 1 
+                }//if statement 3
         
-            }//ends second if statement 
-            else{
-                if(myScanner.hasNext()){    //if user enters a String
+            }//if statement2
+            
+            else{//else statment 2  //when result is false
+                if(myScanner.hasNext()){    //if statement 5    //if user enters a String
                     answer=myScanner.next();    //sets answer equal to user input
                 
-                        if(answer.equals("f") || answer.equals("F")){//third if statement     //if user enters correct answer
+                        if(answer.equals("f") || answer.equals("F")){//if statement 6     //if user enters correct answer
                             
                                 System.out.println("Correct");
                                 return;//leaves the program
-                        }//ends third if statement
+                        }//if statement 6
                         
-                        else{//first else statement     //when user does not enter correct anwer
+                        else{//else statement 3    //when user does not enter correct anwer
                               
                                 System.out.println("Wrong; try again");//notifies user of wrong answer
                                 return;//leaves the program
-                        }//ends first else statement 
-            }
-        }//ends first if statement
-        }
+                        }//else statement 3
+                }//if statement 5
+            }//else statement 2
+        }//if statment 1
         
-        if(value4==2 && value5==2){//fourth if statement    //with two || operators
+        
+        if(value4==2 && value5==2){//if statement 7    //with two || operators
         
             boolean result2=(value1 || value2 || value3);   //result with true/false values
             
             System.out.println("Does " +value1+ " || " +value2+ " || " +value3+ " have the value true(t/T) or false(f/F)?");    //prompts user to state whether value is true or false
             
-            if(result2==true){//fifth if statement   //if result is true
+            if(result2==true){//if statement 8   //if result is true
                 
-                if(myScanner.hasNext()){    //if user enters string
+                if(myScanner.hasNext()){    //if statement 9    //if user enters string
                     answer=myScanner.next();    //sets answer equal to user input
                 
-                    if(answer.equals("t") || answer.equals("T")){//sixth if statement     //if user enters correct answer
+                    if(answer.equals("t") || answer.equals("T")){//if statement 10     //if user enters correct answer
                         
                             System.out.println("Correct");
                             return;//leaves the program
-                    }//ends sixth if statement
+                    }//if statement 10
                     
-                    else{//second else statement     //when user does not enter correct anwer
+                    else{//else statement 4     //when user does not enter correct anwer
                           
                             System.out.println("Wrong; try again");//notifies user of wrong answer
                             return;//leaves the program
-                    }//ends second else statement 
+                    }//else statement 4 
+    
+                }//if statement 9    
+            }//if statement 8
             
-                    
-                }    
-            }//ends fifth if statement 
-            else{
-                if(myScanner.hasNext()){    //if user enters string
+            else{//else statment 5  //if result is false
+                if(myScanner.hasNext()){    //if statement 11   //if user enters string
                     answer=myScanner.next();    //sets answer equal to user input
                 
-                    if(answer.equals("f") || answer.equals("F")){//sixth if statement     //if user enters correct answer
+                    if(answer.equals("f") || answer.equals("F")){//if statement 12     //if user enters correct answer
                         
                             System.out.println("Correct");
                             return;//leaves the program
-                    }//ends sixth if statement
+                    }//if statement 12
                     
-                    else{//second else statement     //when user does not enter correct anwer
+                    else{//else statement 6     //when user does not enter correct anwer
                           
                             System.out.println("Wrong; try again");//notifies user of wrong answer
                             return;//leaves the program
-                    }//ends second else statement 
+                    }//else statement 6
             
-                    
-                }   
-            }
-        }//ends fourth if statement
+                }//if statement 11   
+            }//else statement 5
+        }//if statement 7
             
             
-        if(value4==1 && value5==2){//seventh if statement    //with one && and one || operator
+        if(value4==1 && value5==2){//if statement 13   //with one && and one || operator
         
             boolean result3=(value1 && value2 || value3);   //result with true/false values
             
             System.out.println("Does " +value1+ " && " +value2+ " || " +value3+ " have the value true(t/T) or false(f/F)?");    //prompts user to state whether value is true or false
             
-            if(result3==true){//eighth if statement   //if result is true
+            if(result3==true){//if statement 14   //if result is true
                 
-                if(myScanner.hasNext()){    //if user enters a string
+                if(myScanner.hasNext()){    //if statement 15   //if user enters a string
                     answer=myScanner.next();    //sets scanner equal to user input
                 
-                    if(answer.equals("t") || answer.equals("T")){//ninth if statement     //if user enters correct answer
+                    if(answer.equals("t") || answer.equals("T")){//if statement 16    //if user enters correct answer
                         
                             System.out.println("Correct");
                             return;//leaves the program
-                    }//ends ninth if statement
+                    }//if statement 16
                     
-                    else{//third else statement     //when user does not enter correct anwer
+                    else{//else statement 7     //when user does not enter correct anwer
                           
                             System.out.println("Wrong; try again");//notifies user of wrong answer
                             return;//leaves the program
-                    }//ends third else statement 
+                    }//else statement 7
                 
                     
-                }    
-            }//ends eight if statement 
-            else{
-                if(myScanner.hasNext()){    //if user enters a string
+                }//if statement 15    
+            }//if statement 14
+            
+            else{//else statement 8     //if result is false
+                if(myScanner.hasNext()){    //if statement 17   //if user enters a string
                     answer=myScanner.next();    //sets scanner equal to user input
                 
-                    if(answer.equals("f") || answer.equals("F")){//ninth if statement     //if user enters correct answer
+                    if(answer.equals("f") || answer.equals("F")){//if statement 18    //if user enters correct answer
                         
                             System.out.println("Correct");
                             return;//leaves the program
-                    }//ends ninth if statement
+                    }//if statement 18
                     
-                    else{//third else statement     //when user does not enter correct anwer
+                    else{//else statement 9     //when user does not enter correct anwer
                           
                             System.out.println("Wrong; try again");//notifies user of wrong answer
                             return;//leaves the program
-                    }//ends third else statement 
-            }
-        }//ends seventh if statement    
-        }    
+                    }//else statement 9
+                    
+                }//if statement 17  
+            }//else statement 8   
+        }//if statment 13    
         
-        if(value4==2 && value5==1){//tenth if statement    //with one && and one || operator
+        if(value4==2 && value5==1){//if statement 19    //with one && and one || operator
         
             boolean result4=(value1 || value2 && value3);   //result with true/false values
             
             System.out.println("Does " +value1+ " || " +value2+ " && " +value3+ " have the value true(t/T) or false(f/F)?");    //prompts user to state whether value is true or false
             
-            if(result4==true){//eleventh if statement   //if result is true
+            if(result4==true){//if statement 20   //if result is true
                 
-                if(myScanner.hasNext()){    //if user equals string
+                if(myScanner.hasNext()){    //if statement 21   //if user equals string
                     answer=myScanner.next();    //sets scanner equal to user input
                 
-                    if(answer.equals("t") || answer.equals("T")){//twelfth if statement     //if user enters correct answer
+                    if(answer.equals("t") || answer.equals("T")){//if statement 22     //if user enters correct answer
                         
                             System.out.println("Correct");
                             return;//leaves the program
-                    }//ends twelfth if statement
+                    }//if statement 22
                     
-                    else{//fourth else statement     //when user does not enter correct anwer
+                    else{//else statement 10    //when user does not enter correct anwer
                           
                             System.out.println("Wrong; try again");//notifies user of wrong answer
                             return;//leaves the program
-                    }//ends fourth else statement 
+                    }//else statement 10
                 
-                }
-            }//ends eleventh if statement 
-            else{
-                if(myScanner.hasNext()){    //if user equals string
+                }//if statement 21
+            }//if statement 20
+            
+            else{//else statement 11    //when result is false
+                if(myScanner.hasNext()){    //if statement 23    //if user equals string
                     answer=myScanner.next();    //sets scanner equal to user input
                 
-                    if(answer.equals("t") || answer.equals("T")){//twelfth if statement     //if user enters correct answer
+                    if(answer.equals("t") || answer.equals("T")){//if statement 24     //if user enters correct answer
                         
                             System.out.println("Correct");
                             return;//leaves the program
-                    }//ends twelfth if statement
+                    }//if statement 24
                     
-                    else{//fourth else statement     //when user does not enter correct anwer
+                    else{//else statement 12     //when user does not enter correct anwer
                           
                             System.out.println("Wrong; try again");//notifies user of wrong answer
                             return;//leaves the program
-                    }//ends fourth else statement 
-            }
-        }//ends tenth if statement
-        }
+                    }//else statement 12
+                }//if statement 23
+            }//else statement 11
+        }//if statement 19
+        
     }//ends main method
 }//ends public class
