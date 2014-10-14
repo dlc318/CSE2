8893b82cde21720bef7283e6153c83e8f38d8f83
dlc318 <dlc318@lehigh.edu>
2014-10-14 04:05:23 +0000

    updated Root

--- hw06/Root.java
+++ hw06/Root.java
@@ -16,60 +16,43 @@ public class Root { //creates public class
         
         Scanner number=new Scanner(System.in);  //creates a Scanner
         
+        System.out.println("Enter a double greater than 0, and I'll calculate its square root- ");
         
-        if(number.hasNextDouble()){
+        if(number.hasNextDouble()) {
+        
+        double x=number.nextDouble(); //sets x equal to user input
             
-            double x=number.nextDouble; //sets x equal to user input
+            if(x<=0){    //when user enters a double less than or equal to zero
             
-            if(x<0){
                 System.out.println("You did not enter a double greater than 0");
                 return; //leaves the program
             }
             
-            else{
+            else{   //when user enters a positive double
                 
                 //creates the range and the middle
-                int low=0,
-                int high=1+x;    
-                int middle=high/2;
+                double low=0;
+                double high=1+x;    
+                double middle=high/2;
             
-                while(true){
+                while((high-low) > (0.0000001*(1+x))) { //continues bisection method until the range is less than high*.0000001
                     
-                    if((middle*middle)>x){
+                    if((middle*middle)>x){  //when the square of middle is greater than x
                 
-                    high=middle;    
+                    high= middle;    
                     }
                     
-                    else{
-                    low=middle;
+                    else{   //when the square of middle is less than x
+                    low= middle;
                     }
                     
-                }    
+                    middle= (high+low)/2;   //final result when the while statement is finished
+        
+                }   
+            //prints out the result
+            System.out.println("The square root of " +x+ " is equal to " + middle);    
             }
-            
-            
-            
-            
-            
-            
-            
-            
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
     }//ends main method
 }
